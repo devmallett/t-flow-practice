229bd4df9483d8736bfe857d2a322adffc283b62
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-11 15:32:30 -0400

    Update indi.py

--- indi.py
+++ indi.py
@@ -48,7 +48,7 @@ for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i], cmap=plt.cm.binary)
     plt.xlabel("Actual: " + class_names[test_labels[i]])
-    plt.title("Prediction: " + class_names[np.argmax(prediction[0])])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
     plt.show()
 
 
