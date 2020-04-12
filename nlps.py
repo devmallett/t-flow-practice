f13d08452f227ae288f283846734618d48685991
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-12 16:48:32 -0400

    Must downgrade numpy in order to work

--- nlps.py
+++ nlps.py
@@ -6,4 +6,21 @@ data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 
-print(train_data[0])
+# Integer encoded words, each of these integers point to a certain word 
+# Given one integer, not nice given the computer, find the mapping given these words 
+# Create your own mappings for words 
+# print(train_data[0])
+
+# print(data[0])
+word_index = keras.datasets.imdb.get_word_index() #what are tuples  
+
+
+# What is going on right here???
+# word_index = {k:(v+3) for k, v in word_index.items()}
+# print(word_index)
+# word_index['<PAD>'] = 0
+# word_index['<START>'] = 1
+# word_index['<UNK>'] = 2
+# word_index['<UNUSED>'] = 3
+
+# for i in range(5):
