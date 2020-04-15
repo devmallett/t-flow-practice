b2bd89e56b4265e7be8924faa9e4ff52aa7b248e
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-15 14:40:17 -0400

    More playground
    Getting the overall theme of how a neural network is designed.
    
    Random playground testing to see how data is handled once we get a hold of it

--- mov.py
+++ mov.py
@@ -1,6 +1,35 @@
 import tensorflow as tf
 from tensorflow import keras 
 import numpy as np 
+import random
+
+
+'''
+Supervised Learning - its called that because we are supervising what our model is able to see, so it can then identify
+later subjects? 
+So here is the overall flow of training data 
+
+1. first you have to load botth data sets
+    this can be traiing data and testing data 
+    
+2. next you have to take your training data and get it into a format that you can read 
+    this is most likely some splitting, slicing, reversing of the array 
+    could also be a time when you have to create an array or a dictionary, all depends 
+    if your data does not have a key value system then you have to develop it 
+        tensor flow has this own 
+        
+3. once you have the data ready to be trained, then you have to model the network you want 
+    the input layer ends up being hwo ever many points of data you are working with 
+    hidden layer is typically around 10~12% of what the input layer is 
+
+4. you may have to run the training simulation more than once to reduce your trainig error 
+
+
+Still have to dive into Reinforcement Learning and what goes into that 
+
+
+
+'''
 
 
 data = keras.datasets.imdb
@@ -11,28 +40,44 @@ data = keras.datasets.imdb
 # that represent the review 
 # have to find the mappings, have to create your own mapping and dictionaries 
 
-print(train_data[0])
+# print(train_data[3])
 
-word_index = data.get_word_index()
+word_index = data.get_word_index() #is not an array
 
 
 # 3 keys that will be special characters for word mapping
-word_index = {k:(v+3) for k, v in word_index.items()}
-word_index["<PAD>"] = 0
-word_index["<START>"] = 1
-word_index["<UNK>"] = 2
-word_index["<UNUSED>"] = 3
+# word_index = {k:(v+3) for k, v in word_index.items()}
+# word_index["<PAD>"] = 0
+# word_index["<START>"] = 1
+# word_index["<UNK>"] = 2
+# word_index["<UNUSED>"] = 3
+
+
+# print(word_index())
+
+# So if you want to find a random number 
+yummy = random.choice(list(word_index.keys()))
+
+print(yummy)
 
 
 # GET VALUES THAT ARE NOT VALID, CAN STORE THEM INTO THE DICTIONARY 
-
 reverse_word_index = dict([(value, key) for (key,value) in word_index.items()])
 
-# print(reverse_word_index)
 
-def decode_reviewed(text):
-    return " ".join([reverse_word_index.get(i, "?") for i in text])
-print(decode_reviewed(test_data[0]))
+
+
+# print(word_index)
+checker = len(reverse_word_index) #88,585 items in this tuple
+# print(checker)
+
+# def decode_reviewed(text):
+#     # takes text in - which by this point ahve been converted from the number value to the text 
+#     # one the text is in, you have to reverse 
+#     return " (SPACE) ".join([reverse_word_index.get(i, "?") for i in text])
+#     # RETURN " ".join([reverse_word_index.get(i, "?") for i in text])
+
+# print(decode_reviewed(train_data[3]))
 
 
 # python mov.py
