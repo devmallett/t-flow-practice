adacc3456c9b0e3620346e4336bc63c71f70aff5
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-20 09:55:26 -0400

    Weather Data
    Start of manipulating weather data

--- partTwo.py
+++ partTwo.py
@@ -12,8 +12,8 @@ data = keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-train_images = train_images/255
-test_images = test_images/255
+train_images = train_images/255.0
+test_images = test_images/255.0
 
 # plt.imshow(train_images[20], cmap=plt.cm.binary)
 # plt.show()
